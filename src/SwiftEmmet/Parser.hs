bba7069412b909fa7fab3ea75e6ede2455f56d81
Yusuke Hosonuma <tobi462@gmail.com>
2021-02-08 14:31:55 +0900

    add Parser

--- src/SwiftEmmet/Parser.hs
+++ src/SwiftEmmet/Parser.hs
@@ -1,11 +1,68 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module SwiftEmmet.Parser where
 
-type Field = String
-type Type = String
-data Property = Var Field Type 
-              | Let Field Type
-              deriving (Show, Eq)
-data Expr = Struct String [Property] deriving (Show, Eq)
+import Data.Char (toLower)
+import Data.Text ( pack, Text, toUpper )
+import Data.Attoparsec.Text
+import Control.Applicative
 
-parse :: String -> Expr
-parse _ = Struct "Person" [Var "name" "String"] 
+type Field = Text
+type Type = Text
+data VariableType = Var | Let deriving (Show, Eq)
+data Property = Property VariableType Field Type deriving (Show, Eq)
+data DataType = Struct Text
+              | Class Text
+              deriving (Show, Eq)
+data Expr = Expr DataType [Property] deriving (Show, Eq)
+
+parseExpr :: Text -> Either Text Expr
+parseExpr s = showParseResult
+    $ parse exprParser s `feed` ""
+
+exprParser :: Parser Expr
+exprParser = Expr
+    <$> dataTypeParser 
+    <*> ((schar '>' *> propertiesParser <* endOfInput) <|> (endOfInput >> pure []))
+
+dataTypeParser :: Parser DataType
+dataTypeParser =
+    (Struct <$> (ichar 'S' *> schar '.' *> word)) <|> 
+    (Class  <$> (ichar 'C' *> schar '.' *> word))
+
+propertiesParser :: Parser [Property]
+propertiesParser = propertyParser `sepBy` schar ','
+
+propertyParser :: Parser Property
+propertyParser = Property <$> variableType <*> (schar '.' *> field) <*> (schar ':' *> typeName)
+    where
+        variableType :: Parser VariableType
+        variableType = (char 'v' >> return Var) <|> (char 'l' >> return Let)
+
+        field :: Parser Text
+        field = pack <$> many1 letter
+
+word :: Parser Text
+word = pack <$> many1 (letter <|> digit)
+
+typeName :: Parser Text
+typeName = resolveAlias <$> word
+
+resolveAlias :: Type -> Type
+resolveAlias t = case toUpper t of
+    "S" -> "String"
+    "I" -> "Int"
+    "D" -> "Double"
+    _   -> t
+
+-- ignore case-sensitive
+ichar :: Char -> Parser Char
+ichar c = schar c <|> schar (toLower c)
+
+-- ignore enclose white-spaces
+schar :: Char -> Parser Char
+schar c = skipSpace *> char c <* skipSpace 
+
+showParseResult :: Show a => Result a -> Either Text a
+showParseResult (Done _ r) = Right r
+showParseResult r = Left . pack $ show r
---
+++ test/ParserSpec.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module ParserSpec where
+
+import Test.HUnit ( (~:), (~?=), Test(TestList) )
+import SwiftEmmet.Parser
+
+parseExprTest :: Test
+parseExprTest = TestList
+    [ "parse test 0" ~:
+        parseExpr "S.Person"
+        ~?= Right (Expr (Struct "Person") [])
+    , "parse test 1" ~:
+        parseExpr "C.Person>l.name:String"
+        ~?= Right (Expr (Class "Person") [Property Let "name" "String"])
+    , "parse test 2" ~:
+        parseExpr "S.Person>l.name:String,v.age:Int,v.weight:Double"
+        ~?= Right (Expr (Struct "Person") [ Property Let "name" "String"
+                                          , Property Var "age" "Int"
+                                          , Property Var "weight" "Double"])
+    , "parse test 3" ~:
+        parseExpr "S.Person>l.name:S,v.age:I,v.weight:D"
+        ~?= Right (Expr (Struct "Person") [ Property Let "name" "String"
+                                          , Property Var "age" "Int"
+                                          , Property Var "weight" "Double"])
+    , "parse test 4" ~:
+        parseExpr "s.Person>l.name:s,v.age:i,v.weight:d"
+        ~?= Right (Expr (Struct "Person") [ Property Let "name" "String"
+                                          , Property Var "age" "Int"
+                                          , Property Var "weight" "Double"])
+    , "parse test 5" ~:
+        parseExpr "c.Person>l.name:s,v.age:i,v.weight:d"
+        ~?= Right (Expr (Class "Person") [ Property Let "name" "String"
+                                         , Property Var "age" "Int"
+                                         , Property Var "weight" "Double"])
+    ]
+
+parseExprWithSpaceTest :: Test
+parseExprWithSpaceTest = TestList
+    [ "parse test 0" ~:
+        parseExpr "S . Person"
+        ~?= Right (Expr (Struct "Person") [])
+    , "parse test 1" ~:
+        parseExpr "C . Person > l . name : String"
+        ~?= Right (Expr (Class "Person") [Property Let "name" "String"])
+    , "parse test 2" ~:
+        parseExpr "S . Person > l . name : String , v . age : Int , v . weight : Double"
+        ~?= Right (Expr (Struct "Person") [ Property Let "name" "String"
+                                          , Property Var "age" "Int"
+                                          , Property Var "weight" "Double"])
+    , "parse test 3" ~:
+        parseExpr "S . Person > l . name : S , v . age : I , v . weight : D"
+        ~?= Right (Expr (Struct "Person") [ Property Let "name" "String"
+                                          , Property Var "age" "Int"
+                                          , Property Var "weight" "Double"])
+    , "parse test 4" ~:
+        parseExpr "s . Person > l . name : s , v . age : i , v . weight : d"
+        ~?= Right (Expr (Struct "Person") [ Property Let "name" "String"
+                                          , Property Var "age" "Int"
+                                          , Property Var "weight" "Double"])
+    , "parse test 5" ~:
+        parseExpr "c . Person > l . name : s , v . age : i , v . weight : d"
+        ~?= Right (Expr (Class "Person") [ Property Let "name" "String"
+                                         , Property Var "age" "Int"
+                                         , Property Var "weight" "Double"])
+    ]
